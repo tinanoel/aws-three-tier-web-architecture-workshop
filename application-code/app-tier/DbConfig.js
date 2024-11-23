5f2001656653211d5f3d9cb96a69ee1991831b66
tinanoel <42848076+tinanoel@users.noreply.github.com>
2024-11-23 20:23:32 +0530

    Update DbConfig.js

--- application-code/app-tier/DbConfig.js
+++ application-code/app-tier/DbConfig.js
@@ -1,6 +1,6 @@
 module.exports = Object.freeze({
-    DB_HOST : '',
-    DB_USER : '',
-    DB_PWD : '',
-    DB_DATABASE : ''
+    DB_HOST : 'database-1.cluster-cf4y8q0u2ahp.ap-south-1.rds.amazonaws.com',
+    DB_USER : 'admin',
+    DB_PWD : 'Mypass$1983',
+    DB_DATABASE : 'webappdb'
 });
